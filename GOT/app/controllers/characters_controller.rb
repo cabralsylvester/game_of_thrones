a1a49d3e69d1ae878631465bb5f54189bea0d6bd
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-02-24 16:29:37 -0500

    commitfinal

--- GOT/app/controllers/characters_controller.rb
+++ GOT/app/controllers/characters_controller.rb
@@ -19,6 +19,22 @@ class CharactersController < ApplicationController
     redirect_to characters_path(@character)
   end
 
+  def edit
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @character = Character.find(params[:id])
+    @character.update(add_params)
+    redirect_to @character
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to characters_path
+  end
+
   private
 
   def add_params
